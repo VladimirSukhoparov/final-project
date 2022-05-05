e97b40eca2206bbdab86c3ee64b390682262b84c
Vladimir-Sukhoparov <sukhoparov.v.n@gmail.com>
2022-05-05 17:42:19 +0500

    Добавил отдельный пост со всей логикой, подправил лист

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from "react";
-import api from "./utils/api";
-import PaginationRounded from "./components/Pagination/Pagination";
-import "./index.css";
-import { List } from "./components/List";
-
-
-  
+import React, { useState, useEffect } from 'react'
+import api from './utils/api'
+import PaginationRounded from './components/Pagination/Pagination'
+import './index.css'
+import { List } from './components/List'
+import ModalContext from './contexts/modalContext'
+import { Route, Routes } from 'react-router-dom'
+import { Post } from './components/Post'
 
 export const App = () => {
-  const [postList, setPostList] = useState([]);
-  const [favorites, setFavorites] = useState(
-    JSON.parse(localStorage.getItem("favorites")) || []
-  );
-
-  useEffect(() => {
-    api
-      .getPosts()
-      .then((res) => setPostList(res))
-      .catch((err) => alert(err));
-  }, []);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(15);
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = postList.slice(indexOfFirstPost, indexOfLastPost);
-
-  return (
+    const [postList, setPostList] = useState([])
+    const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites')) || [])
+    const [modalState, setModalState] = useState({
+        isOpen: false,
+        msg: null,
+    })
     
-      <div className="appContainer">
-        <List
-          list={currentPosts}
-          favorites={favorites}
-          setFavorites={setFavorites}
-        />
-        <PaginationRounded
-          postsPerPage={postsPerPage}
-          totalPosts={postList.length}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-      </div>
-    
-  );
-};
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        api.getInfoUser()
+            .then((user) => setUser(user))
+            .catch((err) => alert(err))
+    }, [])
+
+    useEffect(() => {
+        api.getPosts()
+            .then((res) => setPostList(res))
+            .catch((err) => alert(err))
+    }, [])
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage] = useState(15)
+
+    const indexOfLastPost = currentPage * postsPerPage
+    const indexOfFirstPost = indexOfLastPost - postsPerPage
+    const currentPosts = postList.slice(indexOfFirstPost, indexOfLastPost)
+
+    return (
+        <ModalContext.Provider value={{ modalState, setModalState }}>
+            <div className='appContainer'>
+                <div className='content container'>
+                <Routes>
+                <Route path='/'
+                                element={
+                                    <>
+                    <List list={currentPosts} favorites={favorites} setFavorites={setFavorites} />
+                    <PaginationRounded postsPerPage={postsPerPage} totalPosts={postList.length} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                    </>
+                                }/>
+                    <Route path='posts/:itemID' element={<Post user={user?._id} changeList={setPostList} />} />
+                    </Routes>
+                </div>
+            </div>
+        </ModalContext.Provider>
+    )
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,79 +1,115 @@
-import { config } from "./config";
+import { config } from './config'
 
 const onResponse = (res) => {
-  return res.ok ? res.json() : Promise.reject(`Ошибка : ${res.status}`);
-};
+    return res.ok ? res.json() : Promise.reject(`Ошибка : ${res.status}`)
+}
 
 class Api {
-  constructor({ url, token }) {
-    this._url = url;
-    this._token = token;
-  }
+    constructor({ url, token }) {
+        this._url = url
+        this._token = token
+    }
 
-  getPosts(postID) {
-    const requestUrl = postID ? `${this._url}/posts/${postID}` : `${this._url}/posts/`
-    return fetch(requestUrl, {
-        headers: {
-            authorization: `Bearer ${this._token}`,
-        },
-    }).then(onResponse)
+    getPosts(postID) {
+        const requestUrl = postID ? `${this._url}/posts/${postID}` : `${this._url}/posts/`
+        return fetch(requestUrl, {
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    addPost(post) {
+        return fetch(`${this._url}/posts`, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(post),
+        }).then(onResponse)
+    }
+
+    deletePost(postID) {
+        return fetch(`${this._url}/posts/${postID}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    getCurrentUser() {
+        return fetch(`${this._url}/users/me`, {
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    getUserById(userID) {
+        const requestUrl = userID ? `${this._url}/users/${userID}` : `${this._url}/users/`
+        return fetch(requestUrl, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    addLike(itemID) {
+        return fetch(`${this._url}/posts/likes/${itemID}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    deleteLike(itemID) {
+        return fetch(`${this._url}/posts/likes/${itemID}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    getCommentPost(itemID) {
+        return fetch(`${this._url}/posts/comments/${itemID}`, {
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    addComments(itemID, comments) {
+        return fetch(`${this._url}/posts/comments/${itemID}`, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(comments),
+        }).then(onResponse)
+    }
+
+    deleteComments(itemID, commentID) {
+        return fetch(`${this._url}/posts/comments/${itemID}/${commentID}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${this._token}`,
+            },
+        }).then(onResponse)
+    }
+
+    getInfoUser() {
+      return fetch(`${this._url}/users/me`, {
+          headers: {
+              authorization: `Bearer ${this._token}`,
+          },
+      }).then(onResponse)
+  }
 }
 
-  addPost(post) {
-    return fetch(`${this._url}/posts`, {
-      method: "POST",
-      headers: {
-        authorization: `Bearer ${this._token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(post),
-    }).then(onResponse);
-  }
-
-  deletePost(postID) {
-    return fetch(`${this._url}/posts/${postID}`, {
-      method: "DELETE",
-      headers: {
-        authorization: `Bearer ${this._token}`,
-      },
-    }).then(onResponse);
-  }
-
-  getCurrentUser() {
-    return fetch(`${this._url}/users/me`, {
-      headers: {
-        authorization: `Bearer ${this._token}`,
-      },
-    }).then(onResponse);
-  }
-  
-  getUserById(userID) {
-    const requestUrl = userID ? `${this._url}/users/${userID}` : `${this._url}/users/`
-    return fetch(requestUrl, {
-        method: 'GET',
-        headers: {
-            authorization: `Bearer ${this._token}`,
-        },
-    }).then(onResponse)
-  }
-
-  addLike(itemID) {
-    return fetch(`${this._url}/posts/likes/${itemID}`, {
-      method: "PUT",
-      headers: {
-        authorization: `Bearer ${this._token}`,
-      },
-    }).then(onResponse);
-  }
-
-  deleteLike(itemID) {
-    return fetch(`${this._url}/posts/likes/${itemID}`, {
-      method: "DELETE",
-      headers: {
-        authorization: `Bearer ${this._token}`,
-      },
-    }).then(onResponse);
-  }
-}
-
-export default new Api(config);
+export default new Api(config)
