bb7bf3d35cb82db4e8b3b22dda26c1bcbe1001c5
Vladimir-Sukhoparov <sukhoparov.v.n@gmail.com>
2022-05-09 00:27:04 +0500

    Final Project #5

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card } from '../Card'
 import './index.css'
+import { useApi } from '../../hooks/useApi'
 
 export const List = ({ list,changeList, favorites, setFavorites,user }) => {
+    const api=useApi()
+    useEffect(() => {
+                  api.getPosts()
+              .then((list) => {
+                  const listFinal=[];
+              for (let i=list.length-1; i>=0; i--){
+                  listFinal.push(list[i])
+              }
+              changeList(listFinal)}
+              )
+              .catch((err) => alert(err))
+      }, [])
     return (
         <div className='cards'>
             {list?.map((item) => (
